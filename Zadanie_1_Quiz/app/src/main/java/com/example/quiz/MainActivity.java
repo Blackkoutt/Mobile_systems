e44bd7c5c2903b5cdd105d8850912f65566b2388
Mateusz Strapczuk <mateusz.strapczuk@gmail.com>
2023-10-03 17:18:54 +0200

    Adding questions and listening objects for buttons and TextView
    Adding 10 new questions in strings.xml file. Creating a new class Question with fields: questionId and trueAnswer. Adding fields for each button and TextView in MainActivity and binding them to the view elements.

--- Zadanie_1_Quiz/app/src/main/java/com/example/quiz/MainActivity.java
+++ Zadanie_1_Quiz/app/src/main/java/com/example/quiz/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trueButton=findViewById(R.id.true_button);
+        falseButton=findViewById(R.id.false_button);
+        nextButton=findViewById(R.id.next_button);
+        questionTextView=findViewById(R.id.question_text_view);
     }
 }
---
+++ Zadanie_1_Quiz/app/src/main/java/com/example/quiz/Question.java
@@ -0,0 +1,10 @@
+package com.example.quiz;
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+    public Question(int questionId, boolean trueAnswer){
+        this.questionId=questionId;
+        this.trueAnswer=trueAnswer;
+    }
+}
