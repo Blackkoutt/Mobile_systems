f42ad8b27fe8f13bb7666b857f451b4e078df293
Blackkoutt <mateusz.strapczuk@gmail.com>
2023-10-09 12:34:18 +0200

    Initial commit Zadanie_2

---
+++ Zadanie_2/app/src/main/java/com/example/zadanie_2/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.zadanie_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+    private TextView questionNrTextView;
+    private Question[] questions = new Question[]{
+            new Question(R.string.q_language, true),
+            new Question(R.string.q_process, false),
+            new Question(R.string.q_components, false),
+            new Question(R.string.q_resumed_state, true),
+            new Question(R.string.q_on_pause, true),
+            new Question(R.string.q_fragment, false),
+            new Question(R.string.q_service, true),
+            new Question(R.string.q_bound_service, false),
+            new Question(R.string.q_content_provider, false),
+            new Question(R.string.q_broadcast_reciever, false)
+    };
+    private int currentIndex=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trueButton=findViewById(R.id.true_button);
+        falseButton=findViewById(R.id.false_button);
+        nextButton=findViewById(R.id.next_button);
+        questionTextView=findViewById(R.id.question_text_view);
+        questionNrTextView=findViewById(R.id.question_nr_text_view);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswerCorrectness(true);
+            }
+        });
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswerCorrectness(false);
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex+1)%questions.length;
+                setNextQuestion();
+            }
+        });
+        setNextQuestion();
+    }
+    private void setNextQuestion(){
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+        questionNrTextView.setText("Pytanie "+(currentIndex+1)+"/"+questions.length+":");
+    }
+    private void checkAnswerCorrectness(boolean userAnswer){
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+        if(userAnswer == correctAnswer){
+            resultMessageId=R.string.correct_answer;
+        }
+        else{
+            resultMessageId=R.string.incorrect_answer;
+        }
+        Toast.makeText(this,resultMessageId, Toast.LENGTH_SHORT).show();
+
+    }
+}
