be5ae5a38cc11a7b65ffd8cf23fc5ca218aafc22
Mateusz Strapczuk <mateusz.strapczuk@gmail.com>
2023-11-20 14:06:44 +0100

    Initial commit Zadanie 6

---
+++ Zadanie 6 - iOS/Zadanie 6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zadanie 6
+//
+//  Created by student on 13/11/2023.
+//  Copyright © 2023 pb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zadanie 6 - iOS/Zadanie 6/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zadanie 6
+//
+//  Created by student on 13/11/2023.
+//  Copyright © 2023 pb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
